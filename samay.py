87d61c7dcd3450a221d4908c2b076775db728440
Mohit kumar <mohitkumar120904@gmail.com>
2024-09-29 12:44:26 +0530

    Changes in virtual environment for beeping of sound

--- samay.py
+++ samay.py
@@ -1,21 +1,2 @@
 from playsound import playsound
-import time
-
-def alarm(seconds):
-    time_elapsed = 0
-    
-    while time_elapsed < seconds:
-        time.sleep(1)
-        time_elapsed += 1
-
-        time_left = seconds - time_elapsed
-        minutes_left = time_left // 60
-        seconds_left = time_left % 60
-
-        print(f"{minutes_left}:{seconds_left}")
-
-    playsound("Samay-alarm.mp3")
-minutes = int(input("Enter minutes to wait: "))
-seconds = int(input("Enter seconds to wait: "))
-total_seconds = minutes * 60 + seconds
-alarm(total_seconds)
+playsound("samay-alarm.mp3")
