958074738320119c32b27db8e8c6c10502dc7e87
Mohit kumar <mohitkumar120904@gmail.com>
2024-09-29 13:01:52 +0530

    added time attributes

--- samay.py
+++ samay.py
@@ -1,2 +1,12 @@
 from playsound import playsound
+import time
+def samay(seconds):
+    time_elapsed = 0
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
 playsound("samay-alarm.mp3")
